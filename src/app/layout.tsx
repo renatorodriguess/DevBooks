24847cc5dc52462fd0c48ecdfc1a6bf0b4ca659d
Renato Rodrigues <renato072002@gmail.com>
2024-01-21 00:23:40 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,28 @@
+
 import 'react-toastify/dist/ReactToastify.css';
-import './globals.css';
-import React from 'react';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 import { ToastContainer, toast } from 'react-toastify';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'DevBooks',
   description: 'Biblioteca do programador',
   icons: {
-    icon: [''], // Add the correct value for the icon
-  },
-};
-
-export function Message() {
-  const notify = () => toast('Notification Message');
-
-  return (
-    <div>
-      <button onClick={notify}>Notify!</button>
-      <ToastContainer />
-    </div>
-  );
+    icon:['']
+  }
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <React.Fragment>
-      <html lang="pt-br">
-        <body className={inter.className}>{children}</body>
-      </html>
-    </React.Fragment>
-  );
+    <html lang="pt-br">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
 }
