ab6d389932ef1899fd80fb53c6dbb374c31e003a
Renato Rodrigues <renato072002@gmail.com>
2024-01-20 23:37:49 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 
 import 'react-toastify/dist/ReactToastify.css';
 import './globals.css'
+import React from "react"
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ToastContainer, toast } from 'react-toastify';
